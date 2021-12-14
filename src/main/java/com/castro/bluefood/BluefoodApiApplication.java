bfa94f228d33756ecd97229fc8b944bbdf0dd07e
Pedriana <pedrianapavao@gmail.com>
2021-12-14 08:58:21 -0300

    Initial commit

---
+++ src/main/java/com/castro/bluefood/BluefoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.castro.bluefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluefoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluefoodApiApplication.class, args);
+	}
+
+}
